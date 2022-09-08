c6efd77abdde1e2074142b3dace994523bc408c6
Milana-Gulik <milanagulik@mail.ru>
2022-09-08 11:57:47 +0300

    Update Mood Detector Program with const fields

--- src/Task07.java
+++ src/Task07.java
@@ -8,17 +8,27 @@
 import java.util.Random;
 
 public class Task07 {
+    public static final int MOOD_NUMBERS = 5;
+
+    public static final String HAPPY_MOOD = ":)";
+    public static final String BAD_MOOD = ":(";
+    public static final String NEUTRAL_MOOD = ":|";
+    public static final String NERVOUS_MOOD = ":/";
+    public static final String SHOUTED_MOOD = ":()";
+
+
     public static void main(String[] args) {
         Random random = new Random();
 
-        int mood = random.nextInt(5);
-        String msg = ":)";
+        int mood = random.nextInt(MOOD_NUMBERS);
+
+        String msg = HAPPY_MOOD;
 
         switch (mood) {
-            case 1: msg = ":("; break;
-            case 2: msg = ":|"; break;
-            case 3: msg = ":()"; break;
-            case 4: msg = ":/"; break;
+            case 1: msg = BAD_MOOD; break;
+            case 2: msg = NEUTRAL_MOOD; break;
+            case 3: msg = SHOUTED_MOOD; break;
+            case 4: msg = NERVOUS_MOOD; break;
         }
 
         System.out.println(msg);
